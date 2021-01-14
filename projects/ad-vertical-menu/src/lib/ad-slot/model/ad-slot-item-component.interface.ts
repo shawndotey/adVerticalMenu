d47ce4eafc9e313d791183000d1d909877b0cc77
Shawn Dotey <shawncdotey@gmail.com>
2021-01-14 11:57:22 -0500

    remove ad-sot

--- projects/ad-vertical-menu/src/lib/ad-slot/model/ad-slot-item-component.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface AdSlotItemComponent {
-  data: any;
-}
--- projects/ad-vertical-menu/src/lib/ad-slot/model/ad-slot-item.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from '@angular/core';
-export class AdSlotItem {
-  constructor(init: AdSlotItem) {
-    Object.assign(this, init);
-  }
-  component: Type<any>;
-  data: any;
-}
