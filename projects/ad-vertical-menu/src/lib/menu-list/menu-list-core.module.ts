524ff032d7deb0ea31ab73aa96c15bfed87e37df
Shawn Dotey <shawncdotey@gmail.com>
2021-02-03 11:51:21 -0500

    cleanup

--- projects/ad-vertical-menu/src/lib/menu-list/menu-list-core.module.ts
+++ projects/ad-vertical-menu/src/lib/menu-list/menu-list-core.module.ts
@@ -1,14 +1,12 @@
 import { AdMenuListRoutingService } from './menu-list-routing.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdMenuListBuilderService } from './menu-list-builder.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ], providers: [
-    AdMenuListBuilderService,
     AdMenuListRoutingService,
   ]
 })
